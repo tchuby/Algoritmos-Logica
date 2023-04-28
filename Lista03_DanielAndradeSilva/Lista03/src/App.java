ef0d6c52d0f34da33088f3882436f726aafabf0e
tchuby <danielandradesilva.84@gmail.com>
2023-04-28 16:42:40 -0300

    Starting third list

---
+++ Lista03_DanielAndradeSilva/Lista03/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Service service = new Service();
+        service.showPainel();
+    }
+}
---
+++ Lista03_DanielAndradeSilva/Lista03/src/Service.java
@@ -0,0 +1,67 @@
+public class Service {
+    public void showPainel(){
+
+    }
+    public void arrengeIncreasing(){
+        /*
+         * Escreva um algoritmo para imprimir os números
+         * de 1(inclusive) a 10(inclusive) em ordem
+         * crescente.
+         */
+    }
+    public void arrengeDecreasing(){
+        /*
+         * Escreva um algoritmo para imprimir os números
+         * de 1 a 10 (inclusives) em ordem decrescente.
+         */
+    }
+    public void printNumbersGreaterThen(){
+        /*
+         * Escreva um algoritmo para imprimir os 10 
+         * primeiros números inteiros maiores que 100.
+         */
+    }
+    public void printNaturalNumbersUntil(){
+        /*
+         * Ler um valor N e imprimir todos os valores
+         * inteiros entre 1 e N (inclusives). Considere
+         * N será sempre maior que 0.
+         * Modifique o exercício anterio para aceitar 
+         * somente valores maiores que 0 para N. Caso 
+         * o valor informado (para N) não seja maior que
+         * 0, deverá ser lido um novo valor para N.
+         */
+    }
+    public void eightTimesTable(){
+        /*
+         * Escreva um algoritmo que calcule e imprima
+         * a tabuada de 8.
+         */
+    }
+    public void timesTableOf(){
+        /*
+         * Ler um valor inteiro e escrever a tabuada
+         * deste número. (n * 1 a 10)
+         */
+    }
+    public void writeNegativeNumbers(){
+        /*
+         * Ler 10 valores e escrever quantos desses
+         * valores são negativos.
+         */
+    }
+    public void writeNumbersBetween(){
+        /*
+         * Ler 10 valores e escrever quantos desses 
+         * valores estão no intervalo entre 10 e 20
+         * inclusive o 10 e o 20, e quantos estão 
+         * fora do intervalo.
+         */
+    }
+    public void average(){
+        /*
+         * Ler 10 valores e calcular a média aritimética
+         * entre eles.
+         */
+    }
+}
